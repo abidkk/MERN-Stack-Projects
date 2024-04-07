3468fa237e6f5e87d60b118c9b1925ccdfb88022
abidkk <dev.abidkk@gmail.com>
2024-04-07 12:16:00 +0500

    mongodb mongoose connected

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,24 @@
-import express from 'express';
-import {PORT} from "./config.js"
-
+import express from "express";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
 
 const app = express();
 
-app.listen(PORT, ()=>{
-    console.log("App is Running on the Port ", PORT)
-})
+app.get("/", (request, response) => {
+  console.log("Running");
+  console.log(request);
+  response.status(200).send({ message: "Hello World!" });
+});
+
+// Mongodb Connection
+
+mongoose.connect(mongoDBURL);
+try {
+  console.log("Mongo db is connected");
+
+  app.listen(PORT, () => {
+    console.log("App is Running on the Port ", PORT);
+  });
+} catch (error) {
+  console.log(error);
+}
