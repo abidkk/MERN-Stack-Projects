ce2d3c768850bc28215b4872662236cc6efad168
abidkk <dev.abidkk@gmail.com>
2024-04-08 04:54:29 +0500

    cors

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,22 @@
 import express, { request, response } from "express";
+
 import { PORT, mongoDBURL } from "./config.js";
 import { Book } from "./models/bookModel.js";
 import mongoose from "mongoose";
 import booksRoute from "./routes/booksRoute.js"
+import cors from "cors";
+
 const app = express();
 
+app.use(cors());
+// app.use(cors({
+//     origin: 'https://localhost:3002',
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     allowedHeaders: ['Content-Type']
+// }))
 app.use(express.json());
+
+
 // Get Methods
 app.get("/", (request, response) => {
   console.log("Running");
